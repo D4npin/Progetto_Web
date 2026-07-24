a20f3d82da4aadb17d7751d99e34affda018c140
giug04xd <giu.giu040720@gmail.com>
2026-07-24 19:45:54 +0200

    Creata classe "user"

---
+++ app/models/user.py
@@ -0,0 +1,17 @@
+#CLASSE PER "USER"
+from sqlmodel import SQLModel, Field
+
+class User(SQLModel): #la classe base dei modelli del database
+    username: str
+    name: str
+    email: str
+
+class CreateUser(User): #dati richiesti per creare un nuovo utente
+    pass
+
+class UserDB(User, table = True): #la classe che rappresenta la tabella del database
+    __tablename__ = "user" #nome della classe nel database SQL
+    username: str = Field(default=None, primary_key=True)
+
+    class UserPublic(User): #dati restituiti al client
+        pass
