81d8d2d35c9b044337e8c86f0e788c6c926c48e9
D4npin <d.sanna.705@gmail.com>
2026-07-24 19:15:41 +0200

    Creata classe "event"

---
+++ app/models/event.py
@@ -0,0 +1,26 @@
+from sqlmodel import SQLModel, Field
+from datetime import datetime
+
+
+class Event(SQLModel):  #Attributi comuni per tutte le classi
+
+    title: str
+    description: str
+    date: datetime
+    location: str
+
+
+class EventCreate(Event): #Classe da utilizzare nelle POST
+
+    pass
+
+
+class EventPublic(Event): #Classe da utilizzare nelle GET
+
+    id: int  #ID dell'evento
+
+
+class EventDB(Event, table=True): #Collegamento tra codice e DB
+    __tablename__ = "event" #Senza questo il DB assegnerebbe un nome automatico
+                            #invece noi vogliamo esattamente questo nome
+    id: int = Field(default=None, primary_key=True)
